9af813f93c2d8da7d92586dd49f4b5830aab12ca
Wasuthorn-015 <78635169+Wasuthorn-015@users.noreply.github.com>
2021-04-24 15:50:56 +0700

    Add files via upload

---
+++ สอบกลางภาค ข้อ6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace สอบกลางภาคข้อ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte S;
+
+            double Width, Height, Step, I, T, J;
+
+            Console.Write("String = ");
+            S = Byte.Parse(Console.ReadLine());
+
+            Console.Write("Width = ");
+            Width = double.Parse(Console.ReadLine());
+
+            double realwidth = Width;
+            Console.Write("Height = ");
+
+            Height = double.Parse(Console.ReadLine());
+            Console.Write("Step = ");
+
+            Step = double.Parse(Console.ReadLine());
+
+            for (J = 0; J < Step; J++)
+            {
+                for (T = 0; T < Height; T++)
+                {
+                    for (I = 0; I < Width; I++)
+                    {
+                        Console.Write("{0}", S);
+                    }
+                    Console.Write("\n");
+                }
+                Width += realwidth;
+            }
+            Console.ReadLine();
+        }
+    }
+}
