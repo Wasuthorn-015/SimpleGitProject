7567e1b1c9d036f87646f13b47ba481611b68f72
Wasuthorn-015 <78635169+Wasuthorn-015@users.noreply.github.com>
2021-04-30 00:04:24 +0700

    Add files via upload

---
+++ การบ้านครั้งที่4 ข้อ2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace การบ้าน4_ข้อ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool CheckLoopSystem = false;
+
+            while (CheckLoopSystem != true)
+            {
+                char YorN;
+                Console.Write("Please! Input DNA : ");
+
+                string halfDNASequence = Console.ReadLine();
+
+                bool CheckHalfDNASequence = IsValidSequence(halfDNASequence);
+
+                bool CheckLoopHalfDNASequence = false;
+
+                if (CheckHalfDNASequence == true)
+                {
+                    Console.Write("Current half DNA sequence : " + halfDNASequence);
+
+                    while (CheckLoopHalfDNASequence == false)
+                    {
+                        Console.Write("Do you want to replicate it ? (Y or N) : ");
+
+                        YorN = char.Parse(Console.ReadLine());
+
+
+                        if (YorN == 'Y')
+                        {
+                            string y = ReplicateSeqeunce(halfDNASequence);
+
+                            Console.Write("Replicated half DNA sequence: " + y);
+
+                            CheckLoopHalfDNASequence = true;
+                        }
+                        else if (YorN == 'N')
+                        {
+                            CheckLoopHalfDNASequence = true;
+                        }
+                        else
+                        {
+                            Console.Write("Please Input Y or N.");
+                        }
+                    }
+                }
+                else if (CheckHalfDNASequence == false)
+                {
+                    Console.Write("That half DNA sequence is invalid.");
+
+                    while (CheckLoopHalfDNASequence != true)
+                    {
+                        Console.Write("Do you want to process another sequence? (Y or N) : ");
+
+                        YorN = char.Parse(Console.ReadLine());
+                        if (YorN == 'Y')
+                        {
+                            CheckLoopHalfDNASequence = true;
+                        }
+                        else if (YorN == 'N')
+                        {
+                            CheckLoopHalfDNASequence = true;
+
+                            CheckLoopSystem = true;
+                        }
+                        else
+                            Console.Write("Please input Y or N.");
+                    }
+                }
+
+            }
+        }
+
+        static bool IsValidSequence(string halfDNASequence)
+        {
+            foreach (char nucleotide in halfDNASequence)
+            {
+                if (!"ATCG".Contains(nucleotide))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static string ReplicateSeqeunce(string halfDNASequence)
+        {
+            string result = "";
+            foreach (char nucleotide in halfDNASequence)
+            {
+                result += "TAGC"["ATCG".IndexOf(nucleotide)];
+            }
+            return result;
+        }
+    }
+}
