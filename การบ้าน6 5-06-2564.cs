2e58484be0323f8e983025e4db16c369e913cec1
Wasuthorn-015 <78635169+Wasuthorn-015@users.noreply.github.com>
2021-06-05 23:13:48 +0700

    Add files via upload

---
+++ การบ้าน6 5-06-2564.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace การบ้านครั้งที่6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLevel, nMenu, numProblem, Ans;
+            long T1, T2, Time;
+            double S = 0, S1, Qc = 0, Qa = 0, D_Lv = 0;
+            Difficulty LV = Difficulty.Easy;
+
+            Console.Write("Score: {0}, Difficulty: {1}", S, LV);
+            Console.Write("Select Menu 0 / 1 / 2");
+            nMenu = int.Parse(Console.ReadLine());
+            while (nMenu != 2)
+            {
+                switch (nMenu)
+                {
+                    case 0:
+                        T1 = DateTimeOffset.Now.ToUnixTimeSeconds();
+                        if (LV == Difficulty.Easy)
+                        {
+                            numProblem = 3;
+                            for (int i = 0; i < 3; i++)
+                            {
+                                GenerateRandomProblems(numProblem);
+                                Problem[] random = GenerateRandomProblems(numProblem);
+                                Console.Write(random[i].Message);
+                                Console.Write("");
+                                Ans = int.Parse(Console.ReadLine());
+                                if (Ans == random[i].Answer)
+                                {
+                                    Qc = Qc +1
+                                }
+                                Qa = 3;decimal Lv = 0;
+                            }
+                        }
+                }
+            }
+            
+        }
+    }
+}
